ad6214029d9cb915d66293aa758dfa7b400003f1
babizoca <barbarareis009@gmail.com>
2023-08-24 22:45:09 -0300

    aula01

---
+++ aula 01/main.js
@@ -0,0 +1,20 @@
+//console.log eu uso para mostrar uma mensagem ao usuario 
+console .log("hello");
+
+alert('o vitor e chato');
+
+//alert exibe uma mensagem de alerta ao usuário
+
+//para declarar variaveis no js podemos usar "var", "let" ou "const"
+
+const pi = 3.14; 
+alert (pi);
+
+//sempre que usar "const" para declarar uma variável,
+// o valor dela NÃO pode ser alterado
+
+// "let" e uma variável de escopo local
+//"var" e uma variável de escopo global
+
+var nome = "babizoca";
+nome = "maria";
